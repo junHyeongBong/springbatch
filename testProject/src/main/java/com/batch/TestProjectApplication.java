d93cd8314c42ae0f4ab3775137ca86b83b38eb15
dante <heywheredante@gmail.com>
2024-07-14 21:54:11 +0900

    testProject 커밋

---
+++ testProject/src/main/java/com/batch/TestProjectApplication.java
@@ -0,0 +1,14 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+	}
+
+}
